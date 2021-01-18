07e52cae1a4513875dab870b59c481913771a933
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-18 18:36:34 +0100

    added employees state

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,10 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    employees: [],
+  },
+  getters: {},
   mutations: {},
   actions: {},
   modules: {}
