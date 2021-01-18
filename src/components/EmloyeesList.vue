a80b4466376d0a3a82ee4cabd7797c145359a5c9
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-18 09:13:43 +0100

    imported employee-item to list

--- src/components/EmloyeesList.vue
+++ src/components/EmloyeesList.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <h1>Employees</h1>
+    <employee-item />
   </div>
 </template>
 
 <script>
+import EmployeeItem from './EmployeeItem.vue'
 export default {
+  name: 'EmployeesList',
+  components: {
+    EmployeeItem
+  }
 
 }
 </script>
