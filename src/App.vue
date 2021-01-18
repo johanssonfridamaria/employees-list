134a04cb97c966306116dc24bd688c790852d1b0
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-18 19:30:53 +0100

    imported mapActions and mapGetters and added getEmployees and employees. calling get employees when created.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="container">
     <div class="card mt-5">
-      <emloyees-list />
+      <emloyees-list v-if="employees"/>
     </div>
   </div>
 </template>
 
 <script>
 import EmloyeesList from './components/EmloyeesList.vue';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'App',
   components: {
     EmloyeesList,
+  },
+   methods: {
+    ...mapActions(['getEmployees']),
+  },
+  computed: {
+    ...mapGetters(['employees'])
+  },
+  created() {
+    this.getEmployees();
   },
 };
 </script>
