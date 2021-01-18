02a50af751a9ae7f995be3ec4f44489f628c70bc
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-18 09:04:18 +0100

    imported EmployeesList comp to app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="container">
+    <emloyees-list/>
   </div>
 </template>
 
 <script>
+import EmloyeesList from './components/EmloyeesList.vue';
 
 export default {
   name: "App",
   components: {
+    EmloyeesList
   }
 };
 </script>
