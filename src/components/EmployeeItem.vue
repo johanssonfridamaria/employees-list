4ab32e5d8f00f26f6dd07ccd324f24b209dfd96d
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-19 14:27:06 +0100

    removed bold tex, added rounded corners to outer td

--- src/components/EmployeeItem.vue
+++ src/components/EmployeeItem.vue
@@ -1,16 +1,16 @@
 <template>
   <tr>
-    <th scope="row icon" class="text-secondary">
+    <td scope="row" class="icon text-secondary">
       <i class="fas fa-user-circle"></i>
-    </th>
-    <td class="name fw-bold">
+    </td>
+    <td class="name">
       <div class="d-flex name-container">
         <p class="mb-0 me-1">{{ employee.employee_name.split(' ')[0] }}</p>
         <p class="mb-0">{{ employee.employee_name.split(' ')[1] }}</p>
       </div>
     </td>
-    <td class="age fw-bold">{{ employee.employee_age }}</td>
-    <td class="salary text-end fw-bold">{{ employee.employee_salary }} SEK</td>
+    <td class="age text-center">{{ employee.employee_age }}</td>
+    <td class="salary text-end">{{ employee.employee_salary }} SEK</td>
   </tr>
 </template>
 
@@ -27,19 +27,29 @@ export default {
   color: #4d8e91;
 }
 tr {
-    background: #fff;
+    background: transparent;
 }
-.age, .salary, th{
+.age,
+.salary,
+.icon {
   padding: 1rem;
+  background: #fff;
 }
-.name{
+.salary {
+  border-radius: 0 0.5rem 0.5rem 0;
+}
+.icon {
+  border-radius: 0.5rem 0 0 0.5rem;
+}
+.name {
   padding: 1rem 1rem 1rem 0;
+  background: #fff;
 }
 @media (max-width: 576px) {
   .name-container {
     flex-direction: column !important;
   }
-  td{
+  td {
     font-size: 0.9rem;
   }
 }
