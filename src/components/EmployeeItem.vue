89198c7ba101e63501b3a42e6082287e515f14f2
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-01-19 11:08:51 +0100

    created div with names to change layout on smaller screens

--- src/components/EmployeeItem.vue
+++ src/components/EmployeeItem.vue
@@ -3,9 +3,14 @@
     <th scope="row" class="text-secondary">
       <i class="fas fa-user-circle"></i>
     </th>
-    <td>{{ employee.employee_name }}</td>
-    <td>{{ employee.employee_age }}</td>
-    <td>{{ employee.employee_salary }}</td>
+    <td class="name fw-bold">
+      <div class="d-flex flex-column">
+        <p class="mb-0">{{ employee.employee_name.split(' ')[0] }}</p>
+        <p class="mb-0">{{ employee.employee_name.split(' ')[1] }}</p>
+      </div>
+    </td>
+    <td class="age fw-bold">{{ employee.employee_age }}</td>
+    <td class="salary text-end fw-bold">{{ employee.employee_salary }} SEK</td>
   </tr>
 </template>
 
@@ -23,9 +28,6 @@ export default {
 tr {
   border-radius: 0.5rem;
   box-shadow: 0 0 0 1px #ccc;
-}
-td, th{
-  padding: 1rem 0.5rem;
 }
 @media (max-width: 576px) {
   td {
